b2e4c0d5d501860b76d65e1cd28e7899bcf076fe
sophn11 <sophianakpil@gmail.com>
2016-07-22 16:30:03 -0700

    fixed front end and added style to index. having trouble loading handlebars

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,18 @@
-// $(document).ready(function() {
-//   console.log('app.js loaded!');
-//   $.get('/api/articles').success(function (articles) {
-//     articles.forEach(function(article) {
-//       renderArticle(article);
-//     });
-//   });
-//
-// });
+$(document).ready(function() {
+  console.log('app.js loaded!');
+  $.get('/api/articles').success(function (articles) {
+    articles.forEach(function(article) {
+      renderArticle(article);
+    });
+  });
+
+});
+
+// this function takes a single album and renders it to the page
+function renderArticle(article) {
+  console.log('rendering article', article);
+  var articleHtml = $('#article-template').html();
+  var articleTemplate = Handlebars.compile(articleHtml);
+  var html = articleTemplate(article);
+  $('#article').prepend(html);
+}
