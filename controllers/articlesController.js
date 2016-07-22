41c37190b171d213b1800735bcc7c1928bfa382d
sophn11 <sophianakpil@gmail.com>
2016-07-21 21:14:39 -0700

    required models on api and article controller

---
+++ controllers/articlesController.js
@@ -0,0 +1,11 @@
+var db = require('../models');
+// GET /api/albums
+function index(req, res) {
+  db.Article.find({}, function(err, allArticles){
+    if (err) {
+      res.send(404);
+      return;
+    }
+    res.json(allArticles);
+  });
+  }
