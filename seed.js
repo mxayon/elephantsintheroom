e5cf144915562dc736e13ec0b1af042a440aaba5
sophn11 <sophianakpil@gmail.com>
2016-07-21 19:26:40 -0700

    added data to seed.js

---
+++ seed.js
@@ -0,0 +1,40 @@
+var db = require('./models');
+
+var articles_list = [
+  {
+  article_url: "To Kill a Mockingbird",
+  title: "Harper Lee",
+  image_url: "https://s3-us-west-2.amazonaws.com/sandboxapi/to_kill_a_mockingbird.jpg",
+  author: "July 11, 1960",
+  location: " ",
+  date: " ",
+  source: " ",
+  people: " ",
+  tag_words: " ",
+  mood: " "
+  },
+  {
+  article_url: "To Kill a Mockingbird",
+  title: "Harper Lee",
+  image_url: "https://s3-us-west-2.amazonaws.com/sandboxapi/to_kill_a_mockingbird.jpg",
+  author: "July 11, 1960",
+  location: " ",
+  date: " ",
+  source: " ",
+  people: " ",
+  tag_words: " ",
+  mood: " "
+  },
+  {
+  article_url: "To Kill a Mockingbird",
+  title: "Harper Lee",
+  image_url: "https://s3-us-west-2.amazonaws.com/sandboxapi/to_kill_a_mockingbird.jpg",
+  author: "July 11, 1960",
+  location: " ",
+  date: " ",
+  source: " ",
+  people: " ",
+  tag_words: " ",
+  mood: " "
+  }
+];
---
+++ server.js
@@ -0,0 +1,33 @@
+// SERVER-SIDE JAVASCRIPT
+
+//require express in our app
+var express = require('express');
+// generate a new express app and call it 'app'
+var app = express();
+var bodyParser =require('body-parser');
+
+// serve static files from public folder
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended : true }));
+// We'll serve jQuery and bootstrap from a local bower cache avoiding CDNs
+// We're placing these under /vendor to differentiate them from our own assets
+app.use('/vendor', express.static(__dirname + '/bower_components'));
+
+
+app.get('/', function homepage (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+
+/**********
+ * SERVER *
+ **********/
+
+// listen on port 3000
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is running on http://localhost:3000/');
+});
