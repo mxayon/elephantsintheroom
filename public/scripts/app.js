d6de4741b31b05631160b522cc852e6c5f760b1f
sophn11 <sophianakpil@gmail.com>
2016-07-24 04:59:31 -0700

    added some style to front end. added images

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,112 @@
 $(document).ready(function() {
   console.log('app.js loaded!');
   $.get('/api/articles', onSuccess);
+////carousel
+  var clickEvent = false;
+	$('#myCarousel').carousel({
+		interval:   4000
+	}).on('click', '.list-group li', function() {
+			clickEvent = true;
+			$('.list-group li').removeClass('active');
+			$(this).addClass('active');
+	}).on('slid.bs.carousel', function(e) {
+		if(!clickEvent) {
+			var count = $('.list-group').children().length -1;
+			var current = $('.list-group li.active');
+			current.removeClass('active').next().addClass('active');
+			var id = parseInt(current.data('slide-to'));
+			if(count == id) {
+				$('.list-group li').first().addClass('active');
+			}
+		}
+		clickEvent = false;
+	  });
 
+    $("#add_row").on("click", function() {
+          // Dynamic Rows Code
+
+          // Get max row id and set new id
+          var newid = 0;
+          $.each($("#tab_logic tr"), function() {
+              if (parseInt($(this).data("id")) > newid) {
+                  newid = parseInt($(this).data("id"));
+              }
+          });
+          newid++;
+
+          var tr = $("<tr></tr>", {
+              id: "addr"+newid,
+              "data-id": newid
+          });
+
+          // loop through each td and create new elements with name of newid
+          $.each($("#tab_logic tbody tr:nth(0) td"), function() {
+              var cur_td = $(this);
+
+              var children = cur_td.children();
+
+              // add new td and element if it has a nane
+              if ($(this).data("name") != undefined) {
+                  var td = $("<td></td>", {
+                      "data-name": $(cur_td).data("name")
+                  });
+
+                  var c = $(cur_td).find($(children[0]).prop('tagName')).clone().val("");
+                  c.attr("name", $(cur_td).data("name") + newid);
+                  c.appendTo($(td));
+                  td.appendTo($(tr));
+              } else {
+                  var td = $("<td></td>", {
+                      'text': $('#tab_logic tr').length
+                  }).appendTo($(tr));
+              }
+          });
+
+          // add delete button and td
+          /*
+          $("<td></td>").append(
+              $("<button class='btn btn-danger glyphicon glyphicon-remove row-remove'></button>")
+                  .click(function() {
+                      $(this).closest("tr").remove();
+                  })
+          ).appendTo($(tr));
+          */
+
+          // add the new row
+          $(tr).appendTo($('#tab_logic'));
+
+          $(tr).find("td button.row-remove").on("click", function() {
+               $(this).closest("tr").remove();
+          });
+  });
+
+
+
+/////end document ready//////
 });
+
+$("#menu-toggle").click(function(e) {
+       e.preventDefault();
+       $("#wrapper").toggleClass("myCarousel");
+   });
+
+   /*Scroll Spy*/
+   $('body').scrollspy({ target: '#spy', offset:80});
+
+   /*Smooth link animation*/
+   $('a[href*=#]:not([href=#])').click(function() {
+       if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+           var target = $(this.hash);
+           target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+           if (target.length) {
+               $('html,body').animate({
+                   scrollTop: target.offset().top
+               }, 1000);
+               return false;
+           }
+       }
+   });
 
 function renderArticle(article) {
   var articleHtml = $('#article-template').html();
@@ -18,6 +122,14 @@ function onSuccess(json) {
     console.log(article);
   });
 }
+
+$(window).load(function() {
+    var boxheight = $('#myCarousel .carousel-inner').innerHeight();
+    var itemlength = $('#myCarousel .item').length;
+    var triggerheight = Math.round(boxheight/itemlength+1);
+	$('#myCarousel .list-group-item').outerHeight(triggerheight);
+});
+
   // $.get('/api/articles').success(function (articles) {
   //   articles.forEach(function(article) {
   //     renderArticle(article);
