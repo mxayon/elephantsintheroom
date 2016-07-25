adfa2700adf32eeb150a1ad5f9d4433e46f3b0de
sophn11 <sophianakpil@gmail.com>
2016-07-24 17:42:27 -0700

    getting ready to deploy for heroku

--- models/index.js
+++ models/index.js
@@ -6,3 +6,6 @@ module.exports.Article = require('./article.js');
 module.exports.TagWord = require('./tagWord.js');
 
 module.exports.ArticleTagWord = require('./articleTagWord.js');
+
+
+mongoose.connect( process.env.MONGODB_URI || "YOUR CURRENT LOCALHOST DB CONNECTION STRING HERE" );
